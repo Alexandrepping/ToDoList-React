010720bb028e83db854429d42d0729d21a297643
jeanDev <jeanmauricio96@gmail.com>
2020-08-31 22:28:29 -0300

    [Create] To do List

--- src/App.js
+++ src/App.js
@@ -1,67 +1,16 @@
-import React, { useState, useEffect } from "react";
-import api from "./services/api";
-
+import React, { Component } from "react";
 import "./App.css";
 
-import Header from "./components/Header";
+import Tasks from "./components/Tasks";
 
-function App() {
-  const [projects, setProjects] = useState([]);
-
-  const [title, setTitle] = useState("");
-  const [owner, setOwner] = useState("");
-
-  useEffect(() => {
-    api
-      .get("/projects")
-      .then((response) => setProjects(response.data))
-      .catch((error) => console.log(error));
-  }, []);
-
-  async function handleAddProject() {
-    const { data: project } = await api.post("/projects", {
-      title,
-      owner,
-    });
-
-    setProjects([...projects, project]);
-    setTitle("");
-    setOwner("");
-  }
-
-  return (
-    <>
-      <Header title="Projects" />
-
-      <ul>
-        {projects.map((project) => (
-          <li key={project.id}>
-            {project.title} - <b>{project.owner}</b>
-          </li>
-        ))}
-      </ul>
-
-      <div className="inputContainer">
-        <input
-          type="text"
-          value={title}
-          onChange={(e) => setTitle(e.target.value)}
-        />
-        <input
-          type="text"
-          value={owner}
-          onChange={(e) => setOwner(e.target.value)}
-        />
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Tasks titulo="To-Do List com React" />
       </div>
-
-      <button type="button" onClick={handleAddProject}>
-        Adicionar Projetos
-      </button>
-
-      <h1>{title}</h1>
-      <h1>{owner}</h1>
-    </>
-  );
+    );
+  }
 }
 
 export default App;
