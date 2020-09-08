f256010389a7b49475ce2d481a499824549bf9a2
jeanDev <jeanmauricio96@gmail.com>
2020-09-08 00:09:35 -0300

    [Update] Atualização de código

---
+++ src/components/TodoForm.js
@@ -0,0 +1,40 @@
+import { Button, TextField } from "@material-ui/core";
+import React, { useState } from "react";
+import uuid from "uuid";
+
+function TodoForm({ addTodo }) {
+  const [todo, setTodo] = useState({
+    id: "",
+    task: "",
+    completed: false,
+  });
+
+  function handleTaskInputChange(e) {
+    setTodo({ ...todo, task: e.target.value });
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    if (todo.task.trim()) {
+      addTodo({ ...todo, id: uuid.v4() });
+      setTodo({ ...todo, task: "" });
+    }
+  }
+
+  return (
+    <form className="todo-form" onSubmit={handleSubmit}>
+      <TextField
+        label="Your Task"
+        type="text"
+        name="task"
+        value={todo.task}
+        onChange={handleTaskInputChange}
+      />
+      <Button class="button" type="submit">
+        Submit
+      </Button>
+    </form>
+  );
+}
+
+export default TodoForm;
